ce20c2fb716c51a3a6b76fc5062c9db8a28f1026
jignesh1993 <jignesh.mahesh07@gmail.com>
2023-01-10 22:00:55 +0530

    work on like and delete post

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -1,19 +1,20 @@
 import * as api from '../api';
+import { FETCH_ALL, CREATE, UPDATE, DELETE  } from '../constants/actionTypes';
 
 //Action creators
 export const getPosts = () => async (dispatch) => {
     try {
         const { data } = await api.fetchPosts();
-        dispatch({ type: 'FETCH_ALL', payload: data });
+        dispatch({ type: FETCH_ALL, payload: data });
     } catch (error) {
-        console.log('error ->>', error.message); 
+        console.log('error ->>', error.message);
     }
 }
 
 export const createPost = (post) => async (dispatch) => {
     try {
         const { data } = await api.createPost(post);
-        dispatch({ type: 'CREATE', payload: data });
+        dispatch({ type: CREATE, payload: data });
     } catch (error) {
         console.log('error ->>', error.message);  
     }
@@ -22,7 +23,25 @@ export const createPost = (post) => async (dispatch) => {
 export const updatePost = (id, post) => async (dispatch) => {
     try {
         const { data } = await api.updatePost(id, post);
-        dispatch({ type: 'UPDATE', payload: data });
+        dispatch({ type: UPDATE, payload: data });
+    } catch (error) {
+        console.log('error ->>', error.message);  
+    }
+}
+
+export const deletePost = (id) => async (dispatch) => {
+    try {
+        await api.deletePost(id);
+        dispatch({ type: DELETE, payload: id });
+    } catch (error) {
+        console.log('error ->>', error.message);  
+    }
+}
+
+export const likePost = (id) => async (dispatch) => {
+    try {
+        const { data } = await api.likePost(id);
+        dispatch({ type: UPDATE, payload: data });
     } catch (error) {
         console.log('error ->>', error.message);  
     }
--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -6,16 +6,19 @@ import {
   CardMedia,
   Button,
   Typography,
-  colors,
 } from "@material-ui/core";
 import ThumbUpAltIcon from "@material-ui/icons/ThumbUpAlt";
 import DeleteIcon from "@material-ui/icons/Delete";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
 import useStyles from "./styles";
 import moment from "moment";
+import { useDispatch } from "react-redux";
+import { deletePost, likePost } from "../../../actions/posts";
 
 const Post = ({ post, setCurrentId }) => {
   const classes = useStyles();
+  const dispatch = useDispatch();
+
   return (
     <Card className={classes.card}>
       <CardMedia
@@ -43,18 +46,34 @@ const Post = ({ post, setCurrentId }) => {
           {post.tags.map((tag) => `#${tag} `)}
         </Typography>
       </div>
+      <Typography className={classes.title} variant="h5" gutterBottom>
+        {post.title}
+      </Typography>
       <CardContent>
-        <Typography className={classes.title} variant="h5" gutterBottom>
+        <Typography
+          className={classes.message}
+          variant="body2"
+          color="textSecondary"
+          component="p"
+        >
           {post.message}
         </Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button size="small" color="primary" onClick={() => {}}>
-          <ThumbUpAltIcon fontSize="small" />
-          Like
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => dispatch(likePost(post._id))}
+        >
+          <ThumbUpAltIcon fontSize="small" /> &nbsp;
+          Like &nbsp;
           {post.likeCount}
         </Button>
-        <Button size="small" color="primary" onClick={() => {}}>
+        <Button
+          size="small"
+          color="primary"
+          onClick={() => dispatch(deletePost(post._id))}
+        >
           <DeleteIcon fontSize="small" />
           Delete
         </Button>
